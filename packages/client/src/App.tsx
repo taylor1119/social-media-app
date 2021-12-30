9f2d64211c6a584c2471ae53334cbe86a121380a
taylor1119 <taylorcarter1119@gmail.com>
2021-12-30 15:35:49 +0100

    working on client topbar

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,21 @@
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material';
 import React from 'react';
+import TopBar from './components/TopBar';
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>social app</h1>
-		</div>
+		<ThemeProvider theme={darkTheme}>
+			<div className='App'>
+				<TopBar />
+			</div>
+		</ThemeProvider>
 	);
 }
 
