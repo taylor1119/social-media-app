49bf4258aa7e0b2c4598b10369097867418b9311
taylor1119 <taylorcarter1119@gmail.com>
2022-12-09 23:40:24 +0100

    @shared/Added shared module

---
+++ packages/shared/src/types.ts
@@ -0,0 +1,46 @@
+import {
+	IChatMessage,
+	IChatMessageAction,
+	IChatMessageTypingAction,
+	IFriendRequest,
+	IFriendRequestAction,
+	IPost,
+	IPostComment,
+	IUser,
+	IUserConnectionAction,
+} from './interfaces';
+
+export type TChatMessageStatus = 'pending' | 'sent' | 'received' | 'viewed';
+export type TFriendRequestStatus = 'pending' | 'accepted' | 'rejected';
+export type TWebSocketAction =
+	| IChatMessageAction
+	| IChatMessageTypingAction
+	| IUserConnectionAction
+	| IFriendRequestAction;
+
+export type TLoginInput = Pick<IUser, 'password' | 'email'>;
+export type TSignUpInput = Pick<
+	IUser,
+	'userName' | 'email' | 'password' | 'avatar'
+>;
+
+export type TFriendRequestInput = Pick<
+	IFriendRequest,
+	'requester' | 'recipient'
+>;
+
+export type TChatMessageInput = Pick<
+	IChatMessage,
+	'targetId' | 'text' | 'senderId'
+>;
+
+export type TPostInput = Pick<IPost, 'img' | 'description'>;
+
+export type TPostCommentInput = Pick<IPostComment, 'text'>;
+
+export type TUpdateUserInput = Pick<
+	Partial<IUser>,
+	'avatar' | 'cover' | 'email' | 'password' | 'userName' | 'intro'
+>;
+
+export type TMockAccount = Pick<IUser, 'id' | 'avatar' | 'email' | 'userName'>;
