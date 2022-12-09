f02238f194869d90ddf5effd37f9abd3512a867a
taylor1119 <taylorcarter1119@gmail.com>
2022-12-10 00:02:22 +0100

    @client/Added Constants folder

---
+++ packages/client/src/constants/reactQueryKeys.ts
@@ -0,0 +1,24 @@
+import { TPaginatedPostsType } from '../common/types';
+
+const queryKeys = {
+	friends: (friendsOfId?: string) => ['users', 'friends', friendsOfId],
+	friendsIds: 'friends-Ids',
+	receivedFriendRequests: ['friend-requests', 'received'],
+	sentFriendRequests: ['friend-requests', 'sent'],
+	activeFriends: ['users', 'friends', 'active'],
+	conversation: (targetId?: string) => ['messages', 'conversation', targetId],
+	unreadMessages: ['messages', 'unread'],
+	post: (postId?: string) => ['post', postId],
+	user: (userId?: string) => ['user', userId],
+	posts: (type: TPaginatedPostsType, userId?: string) => [
+		'posts',
+		type,
+		userId,
+	],
+	postComments: (postId?: string) => ['post-comments', postId],
+	friendRequestReceivers: ['users', 'friend-request-receiver'],
+	friendRequesters: ['users', 'friend-requesters'],
+	searchUsers: (searchTerm?: string) => ['users', 'search', searchTerm],
+};
+
+export default queryKeys;
