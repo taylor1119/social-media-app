45cfcea2bbe6fece59c07da8b95ba0a03c40f35d
taylor1119 <taylorcarter1119@gmail.com>
2022-12-09 23:43:47 +0100

    @server/Removed unused files

--- packages/server/src/POST/types.ts
+++
@@ -1,4 +0,0 @@
-import { IPost } from './interfaces';
-
-export type TPostDB = Omit<IPost, '_id' | 'createdAt' | 'updatedAt'>;
-export type TPostInput = Omit<TPostDB, 'likes' | 'dislikes' | 'author'>;
