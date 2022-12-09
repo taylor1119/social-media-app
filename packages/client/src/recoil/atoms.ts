0aa6084ab011c4df25e1b2df2340cd64ad281c44
taylor1119 <taylorcarter1119@gmail.com>
2022-12-10 00:04:23 +0100

    @client/Added recoil atoms and effects

---
+++ packages/client/src/recoil/atoms.ts
@@ -0,0 +1,52 @@
+import { atom } from 'recoil';
+import { ITheme } from '../common/interfaces';
+import { TUiUser } from '../common/types';
+import { currentUserPersistEffect, themePersistEffect } from './effects';
+
+export const themeState = atom<ITheme>({
+	key: 'themeState',
+	default: {
+		mode: 'light',
+		isUserPicked: false,
+	},
+	effects: [themePersistEffect],
+});
+
+export const currentUserState = atom<TUiUser | null>({
+	key: 'currentUserState',
+	default: null,
+	effects: [currentUserPersistEffect],
+});
+
+export const leftSideBarOpenState = atom({
+	key: 'leftSideBarOpenState',
+	default: false,
+});
+
+export const friendDetailsOpenState = atom({
+	key: 'friendDetailsOpenStateState',
+	default: false,
+});
+
+export const webSocketState = atom<WebSocket | null>({
+	key: 'webSocketState',
+	default: null,
+});
+
+export const typingIndicatorMapState = atom<Map<string, boolean>>({
+	key: 'typingIndicatorMapState',
+	default: new Map(),
+});
+
+export const chatBoxesState = atom({
+	key: 'chatBoxesState',
+	default: {
+		minimized: new Map<string, TUiUser>(),
+		open: new Map<string, TUiUser>(),
+	},
+});
+
+export const selectedFriendState = atom<TUiUser | null>({
+	key: 'selectedFriendState',
+	default: null,
+});
---
+++ packages/client/src/recoil/effects.ts
@@ -0,0 +1,35 @@
+import { AtomEffect } from 'recoil';
+import { ITheme } from '../common/interfaces';
+import { TUiUser } from '../common/types';
+import { axiosInstance } from '../services/axios';
+
+export const themePersistEffect: AtomEffect<ITheme> = ({ setSelf, onSet }) => {
+	const savedValue = localStorage.getItem('themeState');
+	if (savedValue != null) {
+		setSelf(JSON.parse(savedValue));
+	}
+
+	onSet((newValue, _, isReset) => {
+		isReset
+			? localStorage.removeItem('themeState')
+			: localStorage.setItem('themeState', JSON.stringify(newValue));
+	});
+};
+
+export const currentUserPersistEffect: AtomEffect<TUiUser | null> = ({
+	setSelf,
+	onSet,
+}) => {
+	const savedValue = localStorage.getItem('currentUserState');
+	if (savedValue != null) {
+		setSelf(JSON.parse(savedValue));
+		axiosInstance.defaults.headers.common['csrf-token'] =
+			JSON.parse(savedValue).csrfToken;
+	}
+
+	onSet((newValue, _, isReset) => {
+		isReset
+			? localStorage.removeItem('currentUserState')
+			: localStorage.setItem('currentUserState', JSON.stringify(newValue));
+	});
+};
