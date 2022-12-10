21a576f4949e4e1cb8598c08ce33fab4d80a7434
taylor1119 <taylorcarter1119@gmail.com>
2022-12-10 01:22:00 +0100

    @client/Added profile components

---
+++ packages/client/src/components/Profile/LeftSection.tsx
@@ -0,0 +1,241 @@
+import {
+	Favorite as FavoriteIcon,
+	House as HouseIcon,
+	LocationOn as LocationOnIcon,
+	Person as PersonIcon,
+} from '@mui/icons-material';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import SchoolIcon from '@mui/icons-material/School';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import {
+	Button,
+	ImageList,
+	ImageListItem,
+	ImageListItemBar,
+	Paper,
+	Stack,
+	Typography,
+} from '@mui/material';
+import { debounce } from 'lodash';
+import { useCallback, useRef, useState } from 'react';
+import { InfiniteData } from 'react-query';
+import { Link, Link as RouterLink, useParams } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { queryClient } from '../..';
+import { TPaginatedPost } from '../../common/types';
+import queryKeys from '../../constants/reactQueryKeys';
+import { useGetUserById, useGetUsersById } from '../../hooks/usersHooks';
+import { currentUserState } from '../../recoil/atoms';
+
+const LeftSection = () => {
+	const { userId } = useParams();
+	const currentUser = useRecoilValue(currentUserState);
+	const { data: user } = useGetUserById(userId);
+	const { data: friends } = useGetUsersById(
+		user?.friends,
+		queryKeys.friends(userId)
+	);
+
+	const postsWithImages: TPaginatedPost[] = [];
+	queryClient
+		.getQueryData<InfiniteData<TPaginatedPost>>(
+			queryKeys.posts('timeline', userId)
+		)
+		?.pages.flat()
+		.every((post, idx) =>
+			post.img && idx < 9 ? postsWithImages.push(post) : false
+		);
+
+	const [top, setTop] = useState(0);
+	const containerRef = useRef<HTMLDivElement>(null);
+
+	useCallback(() => {
+		const containerElement = containerRef.current;
+		const debouncedSetTop = debounce(
+			() => setTop(containerElement ? containerElement.clientHeight + 16 : 0),
+			100
+		);
+
+		const resizeObserver = new ResizeObserver(debouncedSetTop);
+		if (containerElement) resizeObserver.observe(containerElement);
+
+		return () => {
+			resizeObserver.disconnect();
+			debouncedSetTop.cancel();
+		};
+	}, [])();
+
+	if (!user) return null;
+
+	return (
+		<Stack
+			ref={containerRef}
+			spacing={2.5}
+			sx={{
+				maxWidth: { xs: '680px', md: '425px' },
+				width: { xs: '95%', md: '360' },
+				position: { xs: 'initial', md: 'sticky' },
+				top: { xs: 'initial', md: `calc(100vh - ${top}px)` },
+			}}
+		>
+			<Paper
+				sx={{
+					p: '16px',
+				}}
+			>
+				<Stack spacing={2.5}>
+					<Stack direction='row' spacing={1} justifyContent='space-between'>
+						<Typography variant='h5' sx={{ fontWeight: 'bold' }}>
+							Intro
+						</Typography>
+						{userId === currentUser?.id && (
+							<Button
+								component={Link}
+								to='/setting/intro'
+								color='primary'
+								variant='text'
+							>
+								Edit
+							</Button>
+						)}
+					</Stack>
+					{user.intro?.bio && (
+						<Stack direction='row' spacing={1}>
+							<PersonIcon />
+							<Typography variant='body1'>Bio: {user.intro?.bio}</Typography>
+						</Stack>
+					)}
+					{user.intro?.work && (
+						<Stack direction='row' spacing={1} alignItems='center'>
+							<BusinessCenterIcon />
+							<Typography variant='body1'>
+								Works at {user.intro?.work}
+							</Typography>
+						</Stack>
+					)}
+					{user.intro?.studiedAt && (
+						<Stack direction='row' spacing={1} alignItems='center'>
+							<SchoolIcon />
+							<Typography variant='body1'>
+								Studied at {user.intro?.studiedAt}
+							</Typography>
+						</Stack>
+					)}
+					{user.intro?.studiesAt && (
+						<Stack direction='row' spacing={1} alignItems='center'>
+							<SchoolIcon />
+							<Typography variant='body1'>
+								Studies at {user.intro?.studiesAt}
+							</Typography>
+						</Stack>
+					)}
+					{user.intro?.from && (
+						<Stack direction='row' spacing={1} alignItems='center'>
+							<LocationOnIcon />
+							<Typography variant='body1'>From {user.intro?.from}</Typography>
+						</Stack>
+					)}
+					{user.intro?.address && (
+						<Stack direction='row' spacing={1} alignItems='center'>
+							<HouseIcon />
+							<Typography variant='body1'>
+								Lives in {user.intro?.address}
+							</Typography>
+						</Stack>
+					)}
+					{user.intro?.relationshipStatus && (
+						<Stack direction='row' spacing={1} alignItems='center'>
+							<FavoriteIcon />
+							<Typography variant='body1'>
+								{user.intro?.relationshipStatus}
+							</Typography>
+						</Stack>
+					)}
+					<Stack direction='row' spacing={1} alignItems='center'>
+						<WatchLaterIcon />
+						<Typography variant='body1'>
+							Joined{' '}
+							{new Date(user.createdAt).toLocaleDateString('en-gb', {
+								year: 'numeric',
+								month: 'long',
+							})}
+						</Typography>
+					</Stack>
+				</Stack>
+			</Paper>
+
+			<Paper sx={{ p: '16px' }}>
+				<Stack direction='row' spacing={1} justifyContent='space-between'>
+					<Typography variant='h5' sx={{ fontWeight: 'bold' }}>
+						Photos
+					</Typography>
+					<Button
+						component={RouterLink}
+						to={`/posts/timeline/${userId}?page=0&index=0`}
+						color='primary'
+						variant='text'
+					>
+						See All Photos
+					</Button>
+				</Stack>
+				<ImageList cols={3} sx={{ borderRadius: '8px' }}>
+					{postsWithImages?.map((image) => (
+						<ImageListItem
+							component={RouterLink}
+							key={'post-image-' + image.id}
+							to={`/posts/timeline/${userId}?page=${image.page}&index=${image.index}`}
+						>
+							<img
+								src={image.img}
+								alt='post'
+								crossOrigin='anonymous'
+								loading='lazy'
+							/>
+						</ImageListItem>
+					)) ?? <></>}
+				</ImageList>
+			</Paper>
+
+			<Paper sx={{ p: '16px' }}>
+				<Stack direction='row' spacing={1} justifyContent='space-between'>
+					<Typography variant='h5' sx={{ fontWeight: 'bold' }}>
+						Friends
+					</Typography>
+					<Button
+						component={Link}
+						to={`/friends/${user.id}`}
+						color='primary'
+						variant='text'
+					>
+						See All Friends
+					</Button>
+				</Stack>
+				<ImageList cols={3} gap={16}>
+					{friends?.slice(0, 9).map((friend) => (
+						<ImageListItem
+							component={RouterLink}
+							key={friend.id}
+							to={`/profile/${friend.id}`}
+							sx={{
+								textDecoration: 'none',
+								color: 'inherit',
+								width: '120px',
+							}}
+						>
+							<img
+								src={friend.avatar}
+								alt={friend.userName}
+								crossOrigin='anonymous'
+								loading='lazy'
+								style={{ borderRadius: '8px' }}
+							/>
+							<ImageListItemBar title={friend.userName} position='below' />
+						</ImageListItem>
+					)) ?? <></>}
+				</ImageList>
+			</Paper>
+		</Stack>
+	);
+};
+
+export default LeftSection;
---
+++ packages/client/src/components/Profile/UpperSection.tsx
@@ -0,0 +1,187 @@
+import { PersonRemove as PersonRemoveIcon } from '@mui/icons-material';
+import ChatIcon from '@mui/icons-material/Chat';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import { Avatar, Box, Button, Stack, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { useParams } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { TUiUser } from '../../common/types';
+import {
+	useAcceptFriendRequest,
+	useGetReceivedFriendRequests,
+	useGetSentFriendRequests,
+	useSendFriendRequest,
+} from '../../hooks/friendRequestsHooks';
+import useChatBox from '../../hooks/useChatBox';
+import {
+	useGetCurrentUserFriendsIds,
+	useGetUserById,
+	useUnfriend,
+} from '../../hooks/usersHooks';
+import { currentUserState } from '../../recoil/atoms';
+
+const StyledBox = styled(Box)(({ theme }) => ({
+	backgroundColor: theme.palette.background.paper,
+}));
+
+const AvatarBox = styled(Box)(({ theme }) => ({
+	backgroundColor: theme.palette.background.paper,
+	border: `4px solid ${theme.palette.background.paper}`,
+}));
+
+const coverStyle: React.CSSProperties = {
+	width: '100%',
+	overflow: 'hidden',
+	display: 'flex',
+	justifyContent: 'center',
+	objectFit: 'cover',
+	height: '350px',
+	borderRadius: '0 0 8px 8px',
+};
+
+const CoverDiv = styled('div')(({ theme }) => ({
+	...coverStyle,
+	backgroundColor: theme.palette.background.default,
+}));
+
+const UpperSection = () => {
+	const { userId } = useParams();
+	const { data: user } = useGetUserById(userId);
+	const currentUser = useRecoilValue(currentUserState);
+	const isCurrentUser = user?.id === currentUser?.id;
+
+	if (!user) return <h1>User Not Found</h1>;
+
+	return (
+		<StyledBox sx={{ width: '100%' }}>
+			<Box
+				sx={{
+					px: { xs: 0, md: '20px' },
+					pb: '20px',
+					width: { xs: '100%', md: '980px' },
+					mx: 'auto',
+				}}
+			>
+				{user.cover ? (
+					<img
+						src={user.cover}
+						alt='cover'
+						style={coverStyle}
+						crossOrigin='anonymous'
+						loading='lazy'
+					/>
+				) : (
+					<CoverDiv />
+				)}
+				<Stack
+					direction={{ xs: 'column', md: 'row' }}
+					justifyContent={{ xs: 'center', md: 'space-between' }}
+				>
+					<Stack
+						direction={{ xs: 'column', md: 'row' }}
+						alignItems={{ xs: 'center', md: 'flex-start' }}
+					>
+						<AvatarBox
+							sx={{
+								width: '150px',
+								height: '150px',
+								mt: '-40px',
+								mx: '40px',
+								borderRadius: '50%',
+							}}
+						>
+							<Avatar
+								alt={user?.userName}
+								src={user?.avatar}
+								sx={{
+									width: '100%',
+									height: '100%',
+								}}
+							/>
+						</AvatarBox>
+
+						<Typography
+							sx={{
+								mt: '20px',
+								fontWeight: 'bold',
+							}}
+							variant='h4'
+							color='#1976d2'
+							gutterBottom
+						>
+							{user?.userName}
+						</Typography>
+					</Stack>
+					{user && !isCurrentUser && <UpperSectionActions user={user} />}
+				</Stack>
+			</Box>
+		</StyledBox>
+	);
+};
+
+const UpperSectionActions = ({ user }: { user: TUiUser }) => {
+	const { data: friendsIds } = useGetCurrentUserFriendsIds();
+	const isFriend = Boolean(friendsIds?.includes(user.id));
+
+	const sentFriendRequest = useGetSentFriendRequests().data?.find(
+		(request) => request.recipient === user.id
+	);
+
+	const receivedFriendRequest = useGetReceivedFriendRequests().data?.find(
+		(request) => request.requester === user.id
+	);
+
+	const useSendFriendRequestResults = useSendFriendRequest(user);
+	const useAcceptFriendRequestResults = useAcceptFriendRequest(user);
+	const useUnfriendResults = useUnfriend();
+
+	const handleLeftButtonClick = () => {
+		if (isFriend) return useUnfriendResults.mutate(user.id);
+		else if (receivedFriendRequest)
+			return useAcceptFriendRequestResults.mutate(receivedFriendRequest.id);
+		else return useSendFriendRequestResults.mutate();
+	};
+
+	const LeftButtonDisabled =
+		useUnfriendResults.isLoading ||
+		useSendFriendRequestResults.isLoading ||
+		useAcceptFriendRequestResults.isLoading ||
+		Boolean(sentFriendRequest);
+
+	const leftButtonText = () => {
+		if (isFriend) return 'unfriend';
+		else if (receivedFriendRequest) return 'Accept friend request';
+		else if (sentFriendRequest) return 'Friend request sent';
+		else return 'Send friend request';
+	};
+
+	const { onOpen } = useChatBox(user);
+
+	return (
+		<Stack
+			alignSelf={{ xs: 'center', md: 'flex-end' }}
+			direction={{ xs: 'column', sm: 'row' }}
+			spacing={2}
+		>
+			<Button
+				startIcon={isFriend ? <PersonRemoveIcon /> : <PersonAddIcon />}
+				variant={isFriend ? 'outlined' : 'contained'}
+				color={isFriend ? 'error' : 'primary'}
+				onClick={handleLeftButtonClick}
+				disabled={LeftButtonDisabled}
+			>
+				{leftButtonText()}
+			</Button>
+			<Button
+				startIcon={<ChatIcon />}
+				variant={isFriend ? 'contained' : 'outlined'}
+				disabled={!isFriend}
+				onClick={onOpen}
+			>
+				Message
+			</Button>
+		</Stack>
+	);
+};
+
+export default UpperSection;
