9546ee1da586e7f9e357bf0f8fba4b1a1896342f
taylor1119 <taylorcarter1119@gmail.com>
2022-12-10 01:26:33 +0100

    @client/Updated App components

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,86 @@
-import { ThemeProvider } from '@emotion/react';
-import { createTheme } from '@mui/material';
-import React from 'react';
-import TopBar from './components/TopBar';
+import {
+	createTheme,
+	CssBaseline,
+	PaletteMode,
+	Theme,
+	ThemeProvider,
+	useMediaQuery,
+} from '@mui/material';
+import { Suspense, useEffect, useMemo } from 'react';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { RouterProvider } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import RecoilDebugButton from './components/Debug/RecoilDebugButton';
+import Loading from './components/Loading';
+import router from './components/router';
+import { useWebSocketInit } from './hooks/useWebSocketInit';
+import { themeState } from './recoil/atoms';
 
-const darkTheme = createTheme({
-	palette: {
-		mode: 'dark',
-	},
-});
+const IS_DEV = process.env.NODE_ENV === 'development';
+
+const getTheme = (mode: PaletteMode): Theme =>
+	createTheme({
+		palette: {
+			mode,
+			background: {
+				default: mode === 'light' ? '#F0F2F5' : '#18191A',
+			},
+		},
+		components: {
+			MuiListItemButton: {
+				styleOverrides: {
+					root: {
+						borderRadius: '8px',
+						':hover': {
+							borderRadius: '8px',
+						},
+					},
+				},
+			},
+			MuiAvatar: {
+				defaultProps: {
+					imgProps: { crossOrigin: 'anonymous', loading: 'lazy' },
+				},
+			},
+		},
+	});
 
 function App() {
+	const [theme, setTheme] = useRecoilState(themeState);
+
+	const prefThemeMode = useMediaQuery('(prefers-color-scheme: dark)')
+		? 'dark'
+		: 'light';
+
+	const muiTheme = useMemo(
+		() => getTheme(theme.isUserPicked ? theme.mode : prefThemeMode),
+		[prefThemeMode, theme]
+	);
+
+	useEffect(() => {
+		if (theme.isUserPicked) return;
+
+		setTheme({
+			isUserPicked: false,
+			mode: prefThemeMode,
+		});
+	}, [prefThemeMode, setTheme, theme.isUserPicked]);
+
+	useWebSocketInit();
+
 	return (
-		<ThemeProvider theme={darkTheme}>
-			<div className='App'>
-				<TopBar />
-			</div>
+		<ThemeProvider theme={muiTheme}>
+			<CssBaseline />
+			<Suspense fallback={<Loading />}>
+				<RouterProvider router={router} />
+			</Suspense>
+			{IS_DEV && (
+				<>
+					<ReactQueryDevtools initialIsOpen={false} />
+					{/* <RecoilTimeTravelObserver /> */}
+					<RecoilDebugButton />
+				</>
+			)}
 		</ThemeProvider>
 	);
 }
