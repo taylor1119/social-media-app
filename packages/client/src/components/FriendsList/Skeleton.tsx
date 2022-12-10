35c23936f91f0bc69c5c72ea8d5e26cdef0b909b
taylor1119 <taylorcarter1119@gmail.com>
2022-12-10 01:24:46 +0100

    @client/Added Friend List components

---
+++ packages/client/src/components/FriendsList/Skeleton.tsx
@@ -0,0 +1,54 @@
+import {
+	Drawer,
+	List,
+	ListItem,
+	ListItemAvatar,
+	ListItemText,
+	Skeleton,
+} from '@mui/material';
+
+const FriendsListSkeleton = () => (
+	<Drawer
+		variant='permanent'
+		anchor='right'
+		sx={{
+			display: { xs: 'none', md: 'block' },
+			width: 330,
+			[`& .MuiDrawer-paper`]: {
+				width: 330,
+				border: 'none',
+			},
+		}}
+	>
+		<List
+			sx={{
+				mt: '76px',
+				'& .MuiButtonBase-root': {
+					borderRadius: '8px',
+					':hover': {
+						borderRadius: '8px',
+					},
+				},
+			}}
+		>
+			{[...Array(15)].map((_, idx) => (
+				<ListItem key={idx} button>
+					<ListItemAvatar>
+						<Skeleton variant='circular' width={40} height={40} />
+					</ListItemAvatar>
+
+					<ListItemText>
+						<Skeleton
+							variant='text'
+							height={40}
+							width={180}
+							sx={{ display: { xs: 'none', md: 'inherit' } }}
+						/>
+					</ListItemText>
+				</ListItem>
+			))}
+		</List>
+	</Drawer>
+);
+
+export default FriendsListSkeleton;
---
+++ packages/client/src/components/FriendsList/index.tsx
@@ -0,0 +1,65 @@
+import { Avatar, ListItemAvatar, ListItemButton } from '@mui/material';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItemText from '@mui/material/ListItemText';
+import { TUiUser } from '../../common/types';
+import useChatBox from '../../hooks/useChatBox';
+import {
+	useGetCurrentUserFriends,
+	useGetOnlineUsersIds,
+} from '../../hooks/usersHooks';
+import ConditionalWrapper from '../ConditionalWrapper';
+import OnlineBadgeWrapper from '../OnlineBadgeWrapper';
+
+const FriendsListItem = ({
+	user,
+	isOnline,
+}: {
+	user: TUiUser;
+	isOnline: boolean;
+}) => {
+	const { onOpen } = useChatBox(user);
+
+	return (
+		<ListItemButton onClick={onOpen}>
+			<ListItemAvatar>
+				<ConditionalWrapper condition={isOnline} wrapper={OnlineBadgeWrapper}>
+					<Avatar src={user.avatar} alt={user.userName} />
+				</ConditionalWrapper>
+			</ListItemAvatar>
+			<ListItemText primary={user.userName} />
+		</ListItemButton>
+	);
+};
+
+const FriendsList = () => {
+	const { data: friends } = useGetCurrentUserFriends();
+	const { data: onlineUsersIds } = useGetOnlineUsersIds();
+
+	return (
+		<Drawer
+			variant='permanent'
+			anchor='right'
+			sx={{
+				display: { xs: 'none', md: 'block' },
+				width: 330,
+				[`& .MuiDrawer-paper`]: {
+					width: 330,
+					border: 'none',
+				},
+			}}
+		>
+			<List sx={{ mx: '8px', mt: '76px' }}>
+				{friends?.map((friend) => (
+					<FriendsListItem
+						key={friend.id}
+						isOnline={onlineUsersIds?.includes(friend.id) ?? false}
+						user={friend}
+					/>
+				))}
+			</List>
+		</Drawer>
+	);
+};
+
+export default FriendsList;
