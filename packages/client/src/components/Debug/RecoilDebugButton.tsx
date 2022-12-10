acaed957314af634d3c32dca8c71044f6e528243
taylor1119 <taylorcarter1119@gmail.com>
2022-12-10 01:19:42 +0100

    @client/Added Debug components

---
+++ packages/client/src/components/Debug/RecoilDebugButton.tsx
@@ -0,0 +1,35 @@
+import { Avatar, Tooltip } from '@mui/material';
+import { useRecoilCallback } from 'recoil';
+
+const RecoilDebugButton = () => {
+	const onClick = useRecoilCallback(
+		({ snapshot }) =>
+			async () => {
+				console.debug('Atom values:');
+				Array.from(snapshot.getNodes_UNSTABLE()).forEach(async (node) => {
+					const value = await snapshot.getPromise(node);
+					console.debug(node.key, value);
+				});
+			},
+		[]
+	);
+
+	return (
+		<Tooltip title='Print current snapshot to console'>
+			<Avatar
+				sx={{
+					position: 'fixed',
+					cursor: 'pointer',
+					left: 64,
+					bottom: 12,
+					zIndex: 99999,
+					opacity: 0.5,
+				}}
+				src='https://recoiljs.org/img/favicon.png'
+				onClick={onClick}
+			/>
+		</Tooltip>
+	);
+};
+
+export default RecoilDebugButton;
---
+++ packages/client/src/components/Debug/RecoilTimeTravelObserver.tsx
@@ -0,0 +1,87 @@
+import { Avatar, Button, Modal, Paper, Tooltip } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Snapshot, useGotoRecoilSnapshot, useRecoilSnapshot } from 'recoil';
+
+function RecoilTimeTravelObserver() {
+	const [snapshots, setSnapshots] = useState<Snapshot[]>([]);
+	const snapshot = useRecoilSnapshot();
+	const gotoSnapshot = useGotoRecoilSnapshot();
+	const release = snapshot.retain();
+
+	useEffect(() => {
+		if (snapshots.every((s) => s.getID() !== snapshot.getID())) {
+			try {
+				setSnapshots((prevSnapshots) => [...prevSnapshots, snapshot]);
+			} finally {
+				release();
+			}
+		}
+	}, [release, snapshot, snapshots]);
+
+	const handleGotoSnapshot = (snapshot: Snapshot) => () => {
+		try {
+			gotoSnapshot(snapshot);
+		} finally {
+			release();
+		}
+	};
+
+	const [openSignUpForm, setOpenSignUpForm] = useState(false);
+	const handleOpenSignUpForm = () => setOpenSignUpForm(true);
+	const handleCloseSignUpForm = () => setOpenSignUpForm(false);
+
+	return (
+		<>
+			<Tooltip title='Time Travel'>
+				<Avatar
+					sx={{
+						position: 'fixed',
+						cursor: 'pointer',
+						right: 12,
+						bottom: 12,
+						zIndex: 9999,
+						opacity: 0.5,
+					}}
+					src='https://recoiljs.org/img/favicon.png'
+					onClick={handleOpenSignUpForm}
+				/>
+			</Tooltip>
+
+			<Modal
+				open={openSignUpForm}
+				onClose={handleCloseSignUpForm}
+				aria-labelledby='sign-up-form'
+				sx={{
+					display: 'flex',
+					justifyContent: 'flex-end',
+					alignItems: 'center',
+				}}
+			>
+				<Paper
+					sx={{
+						p: '15px',
+						display: 'flex',
+						flexDirection: 'column',
+						overflowX: 'hidden',
+						overflowY: 'auto',
+						gap: '5px',
+						height: '500px',
+						width: '225px',
+					}}
+				>
+					{snapshots.map((snapshot, idx) => (
+						<Button
+							key={idx}
+							variant='contained'
+							onClick={handleGotoSnapshot(snapshot)}
+						>
+							Snapshot {idx}
+						</Button>
+					))}
+				</Paper>
+			</Modal>
+		</>
+	);
+}
+
+export default RecoilTimeTravelObserver;
